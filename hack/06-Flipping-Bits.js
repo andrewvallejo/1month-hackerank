6834dc328624abee902516035f2726d4615b6a4c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-10-23 13:38:10 -0600

    Add challenge 6

---
+++ hack/06-Flipping-Bits.js
@@ -0,0 +1,16 @@
+// Flipping bits \\
+// Did not complete due to the lack of understanding on the problem.
+/*
+You will be given a list of 32 bit unsigned integers. 
+Flip all the bits (1 -> 0 and 0 -> 1) 
+and return the result as an unsigned integer.
+
+[i] = 2147483647 | [o] = 2147483648
+[i] = 1          | [o] = 4294967294
+[i] = 0  				 | [o] = 4294967295
+
+*/
+
+var floor = Math.floor;
+var x = parseInt(1, 10)
+console.log(x)
