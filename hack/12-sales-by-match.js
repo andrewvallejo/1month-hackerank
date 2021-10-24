452f3e370253ed11e4ea761a757f3f85d362635a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-10-24 09:42:05 -0600

    Update challenge #12

--- hack/12-sales-by-match.js
+++ hack/12-sales-by-match.js
@@ -11,24 +11,54 @@ Inputs = (7, [1,2,1,2,1,3,2]) || (9, [10,20,20,10,10,30,50,10,20])
 		if !currNUm === num add num
 		 && currNum <= 2 && currNum[0] 
 		 acc++
-		 clearcurrNum
+		 clear currNum
 		 else {
-			 clearcurrNum
+			 clear currNum
 			 set currNUm
 		 }
 
 Outputs = 2										|| 3
 */
 
-console.log('poop')
+
 
 
 const sockMerchant = (n, ar) => {
-	
+	let ph = 0
+	return ar.sort().reduce((acc, curr) => {
+			if (ph === 0) {
+					ph = curr
+			} else if ((ph === curr)) {
+					acc++
+					ph = 0
+			} else if (ph !== curr) {
+					ph = curr
+			}
+			return acc
+	}, 0)
 }
 
 let assert = (a, e) => a === e ? 'Pass' : `Failed: Got ${a} instead of ${e}`
 
 
-let input1 = assert(sockMerchant(7, [1,2,1,2,1,3,2]), 2)
-let input2 = assert(sockMerchant(9, [10,20,20,10,10,30,50,10,20]), 3) 
+let input1 = assert(sockMerchant(7, [1, 2, 1, 2, 1, 3, 2]), 2)
+let input2 = assert(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]), 3)
+
+// console.log(input2)
+
+// Method 2
+let sockMerch = (n, ar) => {
+	let unpaired = new Set()
+	return ar.reduce((acc,sock) => {
+		if(unpaired.has(sock)) {
+			acc++
+			unpaired.delete(sock)
+		} else {
+			unpaired.add(sock)
+		}
+		return acc
+	}, 0)
+
+}
+
+console.log(sockMerch(7, [1, 2, 1, 2, 1, 3, 2]))
