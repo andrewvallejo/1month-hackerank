acbd33f1dc540c0b47ff1fe28e599aac8582b2cb
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-11-02 15:04:38 -0600

    Add dynamic array and its unfinished

---
+++ hack/19-dynamic-array.js
@@ -0,0 +1,10 @@
+// Dynamic Array
+/* 
+  » Declare a 2-dimensional array «» arr, of n empty arrays.
+    - All arrays are zero indexed.
+  » Declare an integer «» lastAnswer (initialized to 0)
+  » There are 2 types of queries (array of strings (to be parsed)).
+    1-Query: 1 x y
+      
+    2-Query: 2 x y
+*/
